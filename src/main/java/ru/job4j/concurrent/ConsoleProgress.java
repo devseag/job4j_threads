2199063825779b137482a2e600a90a4af8acaafe
devseag <example@gmail.com>
2023-08-18 16:51:35 -0300

    add thread stop interrupt

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,25 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        var process = new char[]{'-', '\\', '|', '/' };
+        while (!Thread.currentThread().isInterrupted()) {
+            for (char ch : process) {
+                try {
+                    Thread.sleep(500);
+                    System.out.print("\r load: " + ch);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+}
