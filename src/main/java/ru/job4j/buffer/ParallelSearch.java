4e186c3fd71240197a9b024d3ac766d7ebe767be
devseag <example@gmail.com>
2023-08-19 15:53:58 -0300

    add consumer producer interrupt

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,38 @@
+package ru.job4j.buffer;
+
+import ru.job4j.concurrent.SimpleBlockingQueue;
+
+public class ParallelSearch {
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(5);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+        ).start();
+    }
+}
