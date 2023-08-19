0bc10c1d1f2c84a8dc24225cd0fc389a5554dc4a
devseag <example@gmail.com>
2023-08-18 21:41:15 -0300

    add immutable

---
+++ src/main/java/ru/job4j/linked/Node.java
@@ -0,0 +1,27 @@
+package ru.job4j.linked;
+
+public final class Node<T> {
+    private final Node<T> next;
+    private final T value;
+
+    public Node(Node<T> next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+//    public void setNext(Node<T> next) {
+//        this.next = next;
+//    }
+
+    public T getValue() {
+        return value;
+    }
+
+//    public void setValue(T value) {
+//        this.value = value;
+//    }
+}
