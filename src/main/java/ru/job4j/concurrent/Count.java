0ef5ddf09d45dcc8706ee2e954c4210589a1f338
devseag <example@gmail.com>
2023-08-19 10:22:59 -0300

    add critical section

---
+++ src/main/java/ru/job4j/concurrent/Count.java
@@ -0,0 +1,25 @@
+package ru.job4j.concurrent;
+
+public class Count {
+    private int value;
+
+//    public synchronized void increment() {
+//        value++;
+//    }
+
+    public void increment() {
+        synchronized (this) {
+            value++;
+        }
+    }
+
+//    public synchronized int get() {
+//        return value;
+//    }
+
+    public int get() {
+        synchronized (this) {
+            return value;
+        }
+    }
+}
